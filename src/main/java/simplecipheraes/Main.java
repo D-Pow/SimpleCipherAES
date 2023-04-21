d45e3092eb5ac3cf82118e59d9f46be40131a633
D-Pow <D-Pow@users.noreply.github.com>
2023-04-20 21:59:43 -0400

    Convert to Gradle app; Convert configs to support Java >= 11
    Lots of content here. Since splitting this up into multiple commits would be more confusing than seeing the changes in-bulk (due to how many of the changes are interrelated), this will be a god-commit, but counter-balanced with good code comments (which would be used when diagnosing issues, using this repo as a boilerplate for a new repo, etc.) since comments would be seen and adhered to before commit messages would anyway.

---
+++ src/main/java/simplecipheraes/Main.java
@@ -0,0 +1,9 @@
+package simplecipheraes;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(SimpleCipherGUI.class, args);
+    }
+}
